c07a1db2d11d017e22e7c96892b64ef7eabab930
IsmoilYes <ismoilturdiev1997@gmail.com>
2022-02-01 20:55:59 +0500

    register template

--- article/article/urls.py
+++ article/article/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
+from users.views import register_view
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("polls/",include("polls.urls"))
-]
+    path("polls/",include("polls.urls")),
+    path("register/",register_view)
+
+]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
